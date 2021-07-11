80b68267d9fdb71d2841e006a7261bb4aceb5fd8
Joshua Rully <joshrully@gmail.com>
2021-07-11 11:41:03 -0400

    setup basic api call

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Day from './Day';
 
 function DayList() {
+  const [weather, setWeather] = useState(null)
+
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Guelph&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
 
   return (
     <section className="day-list">
-       
+
     </section>
   );
 }
